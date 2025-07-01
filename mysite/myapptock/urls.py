61e15a650ba1a0951a6ce327a600f6602f3e769c
peebaKU <baramee.254450@gmail.com>
2025-07-02 01:05:45 +0700

    test get and save data to DB

--- mysite/myapptock/urls.py
+++ mysite/myapptock/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('datetime/', views.show_datetime, name='show_datetime'),
     path('tem/', views.my_template_view, name='my_template_view'),
     path('tem2/', views.my_template_view2, name='my_template_view2'),
+    path('cust_profile/', views.cust_profile, name='cust_profile'),
+    path('get_cust_profile/', views.get_cust_profile, name='get_cust_profile'),
 ]
--- mysite/myapptock/views.py
+++ mysite/myapptock/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from myapptock.models import Cust
+from django.contrib import messages
 
 def index(request):
     return HttpResponse("<h1>Welcome to homepage</h1>")
@@ -21,3 +23,33 @@ def my_template_view2(request):
         'tel': "123-456-7890",
     }
     return render(request, 'MyTemplates2.html', template_var)
+
+
+def cust_profile(request):
+    cust = {
+        'name': 'Baramee',
+        'lastname': 'Punyafuaen',
+    }
+    custrecode = Cust()
+    custrecode.name = 'Baramee'
+    custrecode.lastname = 'Punyafuaen'
+    try:
+        custrecode.save()
+        print("Customer record saved successfully.")
+        messages.success(request, "Customer record saved successfully.")
+    except Exception as e:
+        print(f"Error saving customer record: {e}")
+        messages.error(request, f"Error saving customer record: {e}")
+    finally:
+        return render(request, 'cust_profile.html', cust)
+
+
+def get_cust_profile(request):      
+    try:
+        custs = list(Cust.objects.values('name', 'lastname'))
+        messages.success(request, f"Customer found: {custs}")
+    except Cust.DoesNotExist:
+        print("Customer not found.")
+        messages.error(request, "Customer not found.")
+        return render(request, 'cust_profile_filter.html', {'custs': []})
+    return render(request, 'cust_profile_filter.html', {'custs': custs})
